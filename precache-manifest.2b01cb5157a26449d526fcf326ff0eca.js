6f7c1d917ff031a6d64a8caa490833caf5c48922
abdo052 <abdoaborabeh99@gmail.com>
2022-11-07 12:40:29 +0200

    Updates

---
+++ precache-manifest.2b01cb5157a26449d526fcf326ff0eca.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "e46f6bccc7995dbd968e",
+    "url": "/react/static/css/main.122c83e2.chunk.css"
+  },
+  {
+    "revision": "e46f6bccc7995dbd968e",
+    "url": "/react/static/js/main.e46f6bcc.chunk.js"
+  },
+  {
+    "revision": "22b5302c08616ad02822",
+    "url": "/react/static/css/1.727d97b2.chunk.css"
+  },
+  {
+    "revision": "22b5302c08616ad02822",
+    "url": "/react/static/js/1.22b5302c.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "d361b74256b845e6374c15aeac562a03",
+    "url": "/react/static/media/mobile.d361b742.png"
+  },
+  {
+    "revision": "e530e21ca9d5ae4574242c868ca61b15",
+    "url": "/react/static/media/brand3.e530e21c.png"
+  },
+  {
+    "revision": "0055193bb9661adafedbb04493abe8ad",
+    "url": "/react/static/media/clothe.0055193b.png"
+  },
+  {
+    "revision": "2908bf4458e705724b0bbae06f644961",
+    "url": "/react/static/media/avatar.2908bf44.png"
+  },
+  {
+    "revision": "384c0b54a72983f48242143c7ce746ad",
+    "url": "/react/static/media/cat2.384c0b54.png"
+  },
+  {
+    "revision": "1b14400c421b04444cf0fba13cdd8c08",
+    "url": "/react/static/media/labtop.1b14400c.png"
+  },
+  {
+    "revision": "042ab97727532ca317883866f4f611c2",
+    "url": "/react/static/media/sale.042ab977.png"
+  },
+  {
+    "revision": "27072d5cf52087942f3e400ae2ad8587",
+    "url": "/react/static/media/pic.27072d5c.png"
+  },
+  {
+    "revision": "4a0a6c2f682298c030e11b9908e8174f",
+    "url": "/react/static/media/prod1.4a0a6c2f.png"
+  },
+  {
+    "revision": "97b7157205d9bca6b0b49d0a38234aba",
+    "url": "/react/static/media/laptops.97b71572.png"
+  },
+  {
+    "revision": "3be7388292318b2d00e6974f6322ee57",
+    "url": "/react/static/media/slider1.3be73882.png"
+  },
+  {
+    "revision": "201c37e02e8ea009fb1c64c6ab67af1c",
+    "url": "/react/static/media/slider4.201c37e0.png"
+  },
+  {
+    "revision": "bb943f3397ef869175f42aeabf2bd7b3",
+    "url": "/react/static/media/prod3.bb943f33.png"
+  },
+  {
+    "revision": "40d9775a679ee5875725052ce00f6780",
+    "url": "/react/static/media/prod4.40d9775a.png"
+  },
+  {
+    "revision": "4abcf53e103bfce2b9c0f04bf199308d",
+    "url": "/react/static/media/almarai-arabic-400-normal.4abcf53e.woff2"
+  },
+  {
+    "revision": "8f837a16f2bd03b5c108b55c7aef8f29",
+    "url": "/react/static/media/almarai-all-400-normal.8f837a16.woff"
+  },
+  {
+    "revision": "b5415b40980570e19a519270a8b2410a",
+    "url": "/react/index.html"
+  }
+];
